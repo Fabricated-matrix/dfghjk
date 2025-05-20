ced5d97a21adb008233149928ace7619f121aa17
Fabricated-matrix <fabricated.reality.tiime@gmail.com>
2025-05-20 13:36:44 +0530

    Create Footer component

---
+++ src/pages/Pricing.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Pricing() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-4xl font-bold mb-4">Pricing</h1>
+    </div>
+  );
+}
+
+export default Pricing;
